4c0e6d97e92291652fea7afeb2625509fb4e8510
cfryerdev <cfryerdev@gmail.com>
2020-06-12 09:53:41 -0600

    adding some flair to the apps, and fixing a spacing issue in navigation

--- app-about/src/app/root.component.ts
+++ app-about/src/app/root.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { state } from "cfryerdev-dfe-utility-cookiestate";
 
 @Component({
@@ -6,22 +6,22 @@ import { state } from "cfryerdev-dfe-utility-cookiestate";
   templateUrl: "./root.component.html"
 })
 export class RootComponent implements OnInit {
-  username = undefined;
-  userState = undefined;
-  usernameFormat = this.username !== undefined ? `Logged in: ${this.username}` : 'Not logged in.';
+  @Input() username: string;
+  @Input() usernameFormat: string;
 
   links = [
     { name: "Home", url: "http://www.cfryerdev-mfe.info/" },
-    { name: (this.userState !== undefined ? "Logout" : "Login"), url: "http://login.cfryerdev-mfe.info/" }, 
+    { name: (this.username !== '' ? "Logout" : "Login"), url: "http://login.cfryerdev-mfe.info/" }, 
     { name: "About", url: "http://about.cfryerdev-mfe.info/" }, 
     { name: "Products", url: "http://products.cfryerdev-mfe.info/" }
   ];
 
   ngOnInit() { 
     const id = state.getId("DFE-EXAMPLE");
-    this.userState = state.getStateElement(id, "user");
-    if (this.userState !== undefined) {
-      this.username = this.userState.username;
+    const userState = state.getStateElement(id, "user");
+    if (userState !== undefined) {
+      this.username = userState.username;
     }
+    this.usernameFormat = this.username !== undefined ? `Logged in: ${this.username}` : 'Not logged in.';
   }
 }
--- app-home/src/Components/Home/index.jsx
+++ app-home/src/Components/Home/index.jsx
@@ -1,8 +1,52 @@
 import React, { Fragment } from "react";
+import { Line } from 'react-chartjs-2';
+
+const chartData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Activity by Month',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(55,90,127,0.4)',
+      borderColor: 'rgba(55,90,127,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(55,90,127,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(55,90,127,1)',
+      pointHoverBorderColor: 'rgba(55,90,127,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
 
 export default () => (
   <Fragment>
     <h3>Welcome</h3>
-    <p>This is the distributed microfrontend example.</p>
+
+    <div className="card text-white bg-secondary">
+      <div className="card-body">
+      <Line data={chartData} />
+      </div>
+    </div>
+
+    <br />
+
+    <div className="card text-white bg-secondary">
+      <div className="card-body">
+        <h4 className="card-title">Messages</h4>
+        <p className="card-text">
+          <span className="text-muted"> You have no new messages.</span>
+        </p>
+      </div>
+    </div>
   </Fragment>
 );
