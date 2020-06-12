ca061ae50e65a64ec2ba7ddb3d4ea96edb795e79
cfryerdev <cfryerdev@gmail.com>
2020-06-12 16:20:48 -0600

    optimizing angular load times

--- app-about/src/app/app.module.ts
+++ app-about/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from "./app.routes";
 
-import { RootComponent } from "./root.component";
+import { RootComponent } from "./root/root.component";
 import { HomeComponent } from "./home/home.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
