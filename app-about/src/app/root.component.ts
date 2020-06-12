2fc74b3704606a7f225e6133b823c3a2300ea36c
cfryerdev <cfryerdev@gmail.com>
2020-06-12 12:03:15 -0600

    fixes for state

--- app-about/src/app/root.component.ts
+++ app-about/src/app/root.component.ts
@@ -8,20 +8,23 @@ import { state } from "cfryerdev-dfe-utility-cookiestate";
 export class RootComponent implements OnInit {
   @Input() username: string;
   @Input() usernameFormat: string;
-
-  links = [
-    { name: "Home", url: "http://www.cfryerdev-mfe.info/" },
-    { name: (this.username !== '' ? "Logout" : "Login"), url: "http://login.cfryerdev-mfe.info/" }, 
-    { name: "About", url: "http://about.cfryerdev-mfe.info/" }, 
-    { name: "Products", url: "http://products.cfryerdev-mfe.info/" }
-  ];
+  @Input() links: any[];
 
   ngOnInit() { 
     const id = state.getId("DFE-EXAMPLE");
     const userState = state.getStateElement(id, "user");
+    console.log(this.username)
     if (userState !== undefined) {
       this.username = userState.username;
     }
     this.usernameFormat = this.username !== undefined ? `Logged in: ${this.username}` : 'Not logged in.';
+
+    this.links = [
+      { name: "Home", url: "http://www.cfryerdev-mfe.info/" },
+      { name: (this.username !== undefined ? "Logout" : "Login"), url: "http://login.cfryerdev-mfe.info/" }, 
+      { name: "About", url: "http://about.cfryerdev-mfe.info/" }, 
+      { name: "Products", url: "http://products.cfryerdev-mfe.info/" }
+    ];
+
   }
 }
